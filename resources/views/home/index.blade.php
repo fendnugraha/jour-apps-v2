bd2271984098ae4136bb4116d08bb02944e7986d
fendnugraha <fendnugraha92@gmail.com>
2024-05-12 16:10:21 +0700

    tambah edit journal sp deposit belum masih

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -63,36 +63,40 @@
         @php($no = 1)
         @foreach ($accounttrace as $at)
         @php($warna = $at->trx_type == 'Transfer Uang' ? 'table-danger' : 'table-success')
-        @php($hidden = $at->trx_type == 'Voucher & SP' ? 'hidden' : '')
-        @php($status = $at->status == 1 ? '<span class="badge bg-success">Sudah diambil</span>' : '<span
+        @php($hidden = $at->trx_type == 'Voucher & SP' ? 'hidden' : ($at->trx_type == 'Deposit' ? 'hidden' : ''))
+        @php($status = $at->status == 1 ? '<span class="badge bg-success">Success</span>' : '<span
           class="badge bg-warning text-dark">Belum diambil </span>')
-        <tr class="{{ $warna }}">
-          <th scope="row">{{ $no++ }}</th>
-          <td>{{ $at->date_issued }}</td>
-          <td>{{ $at->cred->acc_name }} <i class="fa-solid fa-arrow-right"></i> {{ $at->debt->acc_name }} </td>
-          <td>
-            {!! $status !!} <span class="badge bg-success">{{ $at->trx_type }}</span><br>
+        @php(
+        $badge = ($at->trx_type == 'Transfer Uang') ? 'badge bg-info text-dark' :
+        (($at->trx_type == 'Tarik Tunai') ? 'badge bg-danger' :
+        (($at->trx_type == 'Voucher & SP') ? 'badge bg-primary' :
+        (($at->trx_type == 'Deposit') ? 'badge bg-warning text-dark' : 'badge bg-secondary'))))
+        <th scope="row">{{ $no++ }}</th>
+        <td>{{ $at->date_issued }}</td>
+        <td>{{ $at->cred->acc_name }} <i class="fa-solid fa-arrow-right"></i> {{ $at->debt->acc_name }} </td>
+        <td>
+          {!! $status !!} <span class="badge {{$badge}}">{{ $at->trx_type }}</span><br>
 
-            {{ $at->description }} @if($at->sale){{ $at->sale->product->name . ' - ' . $at->sale->quantity. ' Pcs Harga
-            Modal
-            Rp.'
-            .
-            number_format($at->sale->cost) }}@endif
-          </td>
-          <td>{{ number_format($at->amount) }}</td>
-          <td>{{ number_format($at->fee_amount) }}</td>
-          <td>{{ $at->user->name }}</td>
-          <td>
-            <a href="/home/{{ $at->id }}/edit" class="btn btn-warning btn-sm" {{$hidden}}>
-              <i class="fa-solid fa-pen-to-square"></i></a>
-            <form action="{{ route('accounttrace.delete', $at->id) }}" method="post">
-              @csrf
-              @method('DELETE')
-              <button type="submit" onclick="return confirm('Are you sure?')" class="btn btn-danger btn-sm"><i
-                  class="fa-solid fa-trash"></i>
-              </button>
-            </form>
-          </td>
+          {{ $at->description }} @if($at->sale){{ $at->sale->product->name . ' - ' . $at->sale->quantity. ' Pcs Harga
+          Modal
+          Rp.'
+          .
+          number_format($at->sale->cost) }}@endif
+        </td>
+        <td>{{ number_format($at->amount) }}</td>
+        <td>{{ number_format($at->fee_amount) }}</td>
+        <td>{{ $at->user->name }}</td>
+        <td>
+          <a href="/home/{{ $at->id }}/edit" class="btn btn-warning btn-sm" {{$hidden}}>
+            <i class="fa-solid fa-pen-to-square"></i></a>
+          <form action="{{ route('accounttrace.delete', $at->id) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <button type="submit" onclick="return confirm('Are you sure?')" class="btn btn-danger btn-sm"><i
+                class="fa-solid fa-trash"></i>
+            </button>
+          </form>
+        </td>
         </tr>
         @endforeach
       </tbody>
